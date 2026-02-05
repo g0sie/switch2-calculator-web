ecc28d770d46fbdfc4906ff543d2e0bb5e583088
g0sie <malgorzataderkacz3@gmail.com>
2026-02-05 21:43:37 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="center">
+      <div className="switch">
+        <div className="joycon joycon-left"></div>
+        <div className="screen-border">
+          <div className="screen"></div>
+        </div>
+        <div className="joycon joycon-right"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
